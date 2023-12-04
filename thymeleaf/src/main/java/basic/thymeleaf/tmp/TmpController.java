410e695aec70cc4ab77581a93ab08d1a934d8957
newbe01 <pkwaa3@gmail.com>
2023-12-04 21:26:37 +0900

    date

--- thymeleaf/src/main/java/basic/thymeleaf/tmp/TmpController.java
+++ thymeleaf/src/main/java/basic/thymeleaf/tmp/TmpController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -68,6 +69,13 @@ public class TmpController {
 
         return "basic/basic-objects";
     }
+
+    @GetMapping("date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
 
     @Component("tmpBean")
     static class tmpBean {
