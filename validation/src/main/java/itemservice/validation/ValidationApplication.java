672e22f4c54475b276991d8232ef6b77abe92d5a
newbe01 <pkwaa3@gmail.com>
2023-12-09 03:42:17 +0900

    separate validation

--- validation/src/main/java/itemservice/validation/ValidationApplication.java
+++ validation/src/main/java/itemservice/validation/ValidationApplication.java
@@ -1,13 +1,22 @@
 package itemservice.validation;
 
+import itemservice.validation.web.validation.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+//public class ValidationApplication implements WebMvcConfigurer {
 public class ValidationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ValidationApplication.class, args);
     }
+
+//    @Override
+//    public Validator getValidator() {
+//        return new ItemValidator();
+//    }
 
 }
--- validation/src/main/java/itemservice/validation/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/itemservice/validation/web/validation/ValidationItemControllerV2.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -26,6 +28,11 @@ public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
     private final ItemValidator itemValidator;
+
+//    @InitBinder
+//    public void init(WebDataBinder binder) {
+//        binder.addValidators(itemValidator);
+//    }
 
     @GetMapping
     public String items(Model model) {
@@ -254,7 +261,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(
             @ModelAttribute Item item,
             BindingResult bindingResult,
@@ -263,6 +270,29 @@ public class ValidationItemControllerV2 {
     ) {
 
         itemValidator.validate(item, bindingResult);
+
+        // 실패시 입력폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult : {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공로직
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(
+            @Validated
+            @ModelAttribute Item item,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes,
+            Model model
+    ) {
 
         // 실패시 입력폼으로
         if (bindingResult.hasErrors()) {
