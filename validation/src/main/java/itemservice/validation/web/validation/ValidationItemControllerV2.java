2199632f81c61b721c2248bf89ef27457baf47e0
newbe01 <pkwaa3@gmail.com>
2023-12-09 03:25:11 +0900

    errorcode messaging

--- validation/src/main/java/itemservice/validation/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/itemservice/validation/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -218,6 +219,7 @@ public class ValidationItemControllerV2 {
         log.info("objectName : {}", bindingResult.getObjectName());
         log.info("target : {}", bindingResult.getTarget());
 
+//        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required"); 아래랑 같음
         if (!StringUtils.hasText(item.getItemName())) {
             bindingResult.rejectValue("itemName", "required");
         }
