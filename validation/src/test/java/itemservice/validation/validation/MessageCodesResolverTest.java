0ece6c606d4d72a926c50d15acddadce0ae1a0b1
newbe01 <pkwaa3@gmail.com>
2023-12-09 18:35:52 +0900

    bean validation
    어노테이션을 사용해 validation  을 할 수있다

--- validation/src/test/java/itemservice/validation/validation/MessageCodesResolverTest.java
+++ validation/src/test/java/itemservice/validation/validation/MessageCodesResolverTest.java
@@ -1,4 +1,4 @@
-package itemservice.validation.message.validation;
+package itemservice.validation.validation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
