475b06b973f82dde5eb71de4f1d85aecc308bb2c
newbe01 <pkwaa3@gmail.com>
2023-12-10 23:24:41 +0900

    login - init

---
+++ login/src/main/java/login/itemservice/TestDataInit.java
@@ -0,0 +1,24 @@
+package login.itemservice;
+
+import jakarta.annotation.PostConstruct;
+import login.itemservice.domain.item.Item;
+import login.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
