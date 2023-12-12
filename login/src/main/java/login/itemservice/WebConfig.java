c2c94d79c967347dc364882107588d7928f28b15
newbe01 <pkwaa3@gmail.com>
2023-12-12 14:14:20 +0900

    servlet filter - log

---
+++ login/src/main/java/login/itemservice/WebConfig.java
@@ -0,0 +1,24 @@
+package login.itemservice;
+
+import jakarta.servlet.Filter;
+import login.itemservice.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.setUrlPatterns(Collections.singleton("/*"));
+
+        return filterRegistrationBean;
+    }
+
+}
