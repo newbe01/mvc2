b126660a97805db9eb5e54eaf945b450422c5408
newbe01 <pkwaa3@gmail.com>
2023-12-12 14:54:13 +0900

    spring interceptor - log

--- login/src/main/java/login/itemservice/WebConfig.java
+++ login/src/main/java/login/itemservice/WebConfig.java
@@ -3,16 +3,27 @@ package login.itemservice;
 import jakarta.servlet.Filter;
 import login.itemservice.web.filter.LogFilter;
 import login.itemservice.web.filter.LoginCheckFilter;
+import login.itemservice.web.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Collections;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error");
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
