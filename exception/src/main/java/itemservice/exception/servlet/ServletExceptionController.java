841d6af26d26d69df464ac0bcb0864661f28847c
newbe01 <pkwaa3@gmail.com>
2023-12-13 11:20:14 +0900

    springboot exception handling

--- exception/src/main/java/itemservice/exception/servlet/ServletExceptionController.java
+++ exception/src/main/java/itemservice/exception/servlet/ServletExceptionController.java
@@ -16,6 +16,11 @@ public class ServletExceptionController {
         throw new RuntimeException(" runtime exception");
     }
 
+    @GetMapping("error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 error");
+    }
+
     @GetMapping("error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404 error");
