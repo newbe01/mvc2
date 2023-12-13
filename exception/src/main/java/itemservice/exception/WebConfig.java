e28a09ad0aaf4567a1716a6ccd8690eea478365b
newbe01 <pkwaa3@gmail.com>
2023-12-13 11:01:34 +0900

    exception handling - filter

---
+++ exception/src/main/java/itemservice/exception/WebConfig.java
@@ -0,0 +1,24 @@
+package itemservice.exception;
+
+import itemservice.exception.filter.LogFilter;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterBean = new FilterRegistrationBean<>();
+        filterBean.setFilter(new LogFilter());
+        filterBean.setOrder(1);
+        filterBean.addUrlPatterns("/*");
+//        filterBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        return filterBean;
+    }
+
+}
