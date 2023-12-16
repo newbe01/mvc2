addec668fdcbf68ce670c5d9510bbb32422fbcc0
newbe01 <pkwaa3@gmail.com>
2023-12-16 20:03:55 +0900

    spring formatter
    문자에 특화 되어있음 (문자 > 객체, 객체 > 문자 ) + locale

---
+++ typeConverter/src/test/java/itemservice/typeconverter/formatter/TmpNumberFormatterTest.java
@@ -0,0 +1,30 @@
+package itemservice.typeconverter.formatter;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TmpNumberFormatterTest {
+
+    TmpNumberFormatter formatter = new TmpNumberFormatter();
+
+    @Test
+    void parse() throws ParseException {
+
+        Number result = formatter.parse("1,000", Locale.KOREA);
+
+        assertThat(result).isEqualTo(1000L);
+    }
+
+    @Test
+    void print() {
+
+        String result = formatter.print(1000, Locale.KOREA);
+
+        assertThat(result).isEqualTo("1,000");
+    }
+}
