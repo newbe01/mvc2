57c37fb053cd9c4cc75c02aa7b28bc11f5abc89e
newbe01 <pkwaa3@gmail.com>
2023-12-16 18:10:17 +0900

    type - converter init

---
+++ typeConverter/src/test/java/itemservice/typeconverter/TypeConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package itemservice.typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
