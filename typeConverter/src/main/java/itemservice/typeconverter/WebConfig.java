88c6eb6c90adc2a43539bb1c86119b0bfc677d60
newbe01 <pkwaa3@gmail.com>
2023-12-16 20:15:38 +0900

    formatter
    configuration 등록시 converter 가 formatter 보다 우선순위를 가진다

--- typeConverter/src/main/java/itemservice/typeconverter/WebConfig.java
+++ typeConverter/src/main/java/itemservice/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import itemservice.typeconverter.converter.IntegerToStringConverter;
 import itemservice.typeconverter.converter.IpPortToStringConverter;
 import itemservice.typeconverter.converter.StringToIntegerConverter;
 import itemservice.typeconverter.converter.StringToIpPortConverter;
+import itemservice.typeconverter.formatter.TmpNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,9 +15,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         // 등록한 converter가 우선순위
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIntegerConverter()); formatter 보다 우선순위
+
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
+
+        registry.addFormatter(new TmpNumberFormatter());
     }
+
 }
